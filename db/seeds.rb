a2a20c955654db889ffa813c19c33220251c2d9c
Wesley <wes.beck21@outlook.com>
2021-01-13 12:58:18 -0800

    commit tree ea0f36417be673d25bdadbfe0a2f3515838fe081

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+paul = Student.create(first_name: "Paul", last_name: "Mall")
